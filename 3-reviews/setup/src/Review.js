0c02ce282df37897e225cfc93683305bddcc3865
Pratik Dhungel <dhungel.1996@gmail.com>
2020-11-18 10:55:54 +0545

    Reviews Project: Switch between reviews

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -5,6 +5,30 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
+
+  const loopReviews = (currentIndex) => {
+    if (currentIndex > people.length - 1) {
+      return 0;
+    }
+    if (currentIndex < 0) {
+      return people.length - 1;
+    }
+    return currentIndex;
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return loopReviews(newIndex);
+    });
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return loopReviews(newIndex);
+    });
+  };
+
   return (
     <article className='review'>
       <div className='img-container'>
@@ -17,10 +41,10 @@ const Review = () => {
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
       <div className='button-container'>
-        <button className='prev-btn'>
+        <button className='prev-btn' onClick={() => prevPerson()}>
           <FaChevronLeft />
         </button>
-        <button className='next-btn'>
+        <button className='next-btn' onClick={() => nextPerson()}>
           <FaChevronRight />
         </button>
       </div>
