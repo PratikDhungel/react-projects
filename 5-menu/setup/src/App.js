cf63d18c526f5e3d1f211a47753547e09495d8ae
Pratik Dhungel <dhungel.1996@gmail.com>
2020-11-18 15:28:29 +0545

    Menu Project: Display all items

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -4,7 +4,21 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [categories, setCategories] = useState([]);
+  const [menuItems, setMenuItems] = useState(items);
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
