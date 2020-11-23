2eba07df45357e5628b27df8907a1333811d937e
Pratik Dhungel <dhungel.1996@gmail.com>
2020-11-23 09:52:52 +0545

    Grocery Bud Project: Form Setup

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState('');
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Hello World');
+  };
+
+  return (
+    <section className='section-center'>
+      <form action='' className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className='form-control'>
+          <input type='text' className='grocery' placeholder='E.g. Breads' onChange={(e) => setName(e.target.value)} />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'Edit' : 'Submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>Clear Items</button>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
