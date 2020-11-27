f85733c249057b495576ace12df8b9f1f74bc5ba
Pratik Dhungel <dhungel.1996@gmail.com>
2020-11-27 08:24:18 +0545

    Stripe Submenus Project: Setup Sidebar components

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
           <h1>Payments infrastructure for the internet</h1>
           <p>
             Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept
-            payments, send payouts, and manage their businesses online.{' '}
+            payments, send payouts, and manage their businesses online.
           </p>
           <button className='btn'>Start Now</button>
         </article>
