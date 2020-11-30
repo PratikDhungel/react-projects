749188d7e382f8957e3d75057aa121f02b0a4eb9
Pratik Dhungel <dhungel.1996@gmail.com>
2020-11-30 09:26:10 +0545

    Cocktails Project: Setup context and add Searchform and Cocktails component

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+import { useGlobalContext } from '../context';
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+
+  console.log(loading);
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (cocktails.length < 1) {
+    return <h2 className='section-title'>No Cocktails match your search Criteria</h2>;
+  }
+
   return (
     <div>
       <h2>cocktail list component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default CocktailList
+export default CocktailList;
