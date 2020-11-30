f03bf2c73984499ce0b737af77db25f619f84ed6
Pratik Dhungel <dhungel.1996@gmail.com>
2020-11-30 09:13:57 +0545

    Cocktails Project: Setup About and Error pages

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae eveniet numquam quo nobis repellendus
+        perferendis voluptas veritatis distinctio sunt iure illum natus deserunt maiores exercitationem doloribus dolorem
+        excepturi suscipit vero ab quasi, ipsa ipsam delectus temporibus saepe? Tempore, provident accusamus!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
